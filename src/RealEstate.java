a5616120b5196250adaac59b4e13b04e977ee855
Alik <alikharutyunyan00@gmail.com>
2022-12-21 18:50:02 +0200

    Made changes to classes (mainly User) to make them validate themselves

--- src/RealEstate.java
+++ src/RealEstate.java
@@ -7,38 +7,27 @@ public class RealEstate {
 
     public RealEstate () {
         users = new User[] {
-                new User("Michelle", "11458_", "0529516569", true),
+                new User("Michelle", "11458$", "0529516569", true),
                 new User("Alik", "37777777_", "0533061346", false)
         };
     }
 
     public void createUser () {
+        User newUser = new User();
         Scanner scanner = new Scanner(System.in);
-        String userName;
-        boolean isAvailable;
 
+        String userName;
         do {
-            isAvailable = true;
             System.out.println("Enter the user name:");
             userName = scanner.nextLine();
 
-            for (int i = 0; i < users.length; i++) {
-                if (userName.equals(users[i].getUserName())) {
-                    System.out.println("The user name is already taken");
-                    isAvailable = false;
-                    break;
-                }
+            if(checkIfAvailable(userName)){
+                newUser.setUserName(userName);
             }
-
-            if (isAvailable) {
-                System.out.println("The user name is available");
-            }
-
-        } while (!isAvailable);
+        } while (newUser.getUserName() == null);
 
 
         String userPassword;
-
         System.out.println("The password must contain the following elements: \n" +
                 "- At least 1 character must be a digit \n" +
                 "- Password must be minimum 5 characters long \n" +
@@ -46,90 +35,53 @@ public class RealEstate {
         do {
             System.out.println("Enter your password: ");
             userPassword = scanner.nextLine();
-        } while (!checkPassword(userPassword));
+            newUser.setPassword(userPassword);
+        } while (newUser.getPassword() == null);
+
 
         String phoneNumber;
-        boolean isPhoneNumber;
-
         do {
-            isPhoneNumber = true;
             System.out.println("Enter your phone number");
             phoneNumber = scanner.nextLine();
+            newUser.setPhoneNumber(phoneNumber);
+        } while (newUser.getPhoneNumber() == null);
 
-            if (phoneNumber.length()==10 && phoneNumber.startsWith("05")) {
-                for (int i = 0; i < phoneNumber.length(); i++) {
-                    if (!(containsNumber( "" + phoneNumber.charAt(i)))) {
-                        isPhoneNumber = false;
-                        System.out.println("Phone number is not acceptable, please try again");
-                        break;
-                    }
-                }
-            }
-
-            else {
-                isPhoneNumber = false;
-                System.out.println("Phone number is not acceptable, please try again");
-            }
-
-        } while (!isPhoneNumber);
-
-        boolean isBroker;
-        boolean endLoop = false;
 
         do {
             System.out.println("Are you a broker? Answer yes/no");
             String userAnswer = scanner.nextLine();
             userAnswer = userAnswer.toLowerCase().trim();
 
-            if (userAnswer.equals("yes")) {
-                isBroker = true;
-                System.out.println("you are a broker");
-                endLoop = true;
-            }
-
-            else if (userAnswer.equals("no")) {
-                isBroker = false;
-                System.out.println("you are not a broker");
-                endLoop = true;
-            }
-
-            else {
-                System.out.println("Please answer correctly");
-            }
-
-        } while (!endLoop);
-
-
-    }
-
-
-
-    private boolean checkPassword(String password) {
-        boolean result = false;
-        if (password.length()>=5) {
-            if (password.contains("$") || password.contains("_") || password.contains("%")) {
-                if (containsNumber(password)) {
-                    result = true;
+            if(checkAnswer(userAnswer)){
+                switch(userAnswer){
+                    case "yes" -> newUser.setBroker(true);
+                    case "no" -> newUser.setBroker(false);
                 }
             }
-        }
+        } while (newUser.getBroker() == null);
 
-        if (!result) {
-            System.out.println("The password is not strong enough");
+        System.out.println(newUser);
+    }
+    private boolean checkAnswer(String userAnswer){
+        boolean result = false;
+        if(userAnswer.equals("yes") || userAnswer.equals("no")){
+            result = true;
+        }else{
+            System.out.println("Please answer correctly");
         }
-    return result;
+        return result;
     }
 
-    private boolean containsNumber(String string) {
-        boolean isNumberCheck = false;
-        for (int i = 0; i < 10; i++) {
-            if (string.contains("" + i)) {
-                isNumberCheck = true;
+    private boolean checkIfAvailable(String userName){
+        boolean isAvailable = true;
+        for (int i = 0; i < users.length; i++) {
+            if (userName.equals(users[i].getUserName())) {
+                System.out.println("The user name is already taken");
+                isAvailable = false;
                 break;
             }
         }
-        return isNumberCheck;
+        return isAvailable;
     }
-
 
 }
--- src/User.java
+++ src/User.java
@@ -2,13 +2,22 @@ public class User {
     private String userName;
     private String password;
     private String phoneNumber;
-    private boolean isBroker;
+    private Boolean isBroker;
+
+    public static final int PHONE_NUMBER_LENGTH = 10;
 
     public User (String userName, String password, String phoneNumber, boolean isBroker) {
-        this.userName = userName;
-        this.password = password;
-        this.phoneNumber = phoneNumber;
+        setUserName(userName);
+        setPassword(password);
+        setPhoneNumber(phoneNumber);
         this.isBroker = isBroker;
+    }
+    public User(){
+        this.userName = null;
+        this.password = null;
+        this.phoneNumber = null;
+        this.isBroker = null;
+
     }
     public String getUserName() {
         return userName;
@@ -23,7 +32,34 @@ public class User {
     }
 
     public void setPassword(String password) {
-        this.password = password;
+        if (isValidPassword(password)) {
+           this.password = password;
+        }else{
+            System.out.println("The password is not strong enough");
+        }
+    }
+
+    private boolean isValidPassword(String password){
+        boolean result = false;
+        if (password.length()>=5) {
+            if (password.contains("$") || password.contains("_") || password.contains("%")) {
+                if (containsNumber(password)) {
+                    result = true;
+                }
+            }
+        }
+        return result;
+    }
+
+    private boolean containsNumber(String string) {
+        boolean isNumberCheck = false;
+        for (int i = 0; i < 10; i++) {
+            if (string.contains("" + i)) {
+                isNumberCheck = true;
+                break;
+            }
+        }
+        return isNumberCheck;
     }
 
     public String getPhoneNumber() {
@@ -31,14 +67,47 @@ public class User {
     }
 
     public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
+        if(phoneNumberValidation(phoneNumber)){
+            this.phoneNumber = phoneNumber;
+        }
     }
 
-    public boolean isBroker() {
-        return isBroker;
+    private boolean phoneNumberValidation(String phoneNumber){
+        boolean isPhoneNumber = true;
+        if (phoneNumber.length() == PHONE_NUMBER_LENGTH && phoneNumber.startsWith("05")) {
+            for (int i = 0; i < phoneNumber.length(); i++) {
+                if (!(containsNumber( "" + phoneNumber.charAt(i)))) {
+                    isPhoneNumber = false;
+                    System.out.println("Phone number is not acceptable, please try again");
+                    break;
+                }
+            }
+        }
+
+        else {
+            isPhoneNumber = false;
+            System.out.println("Phone number is not acceptable, please try again");
+        }
+        return isPhoneNumber;
     }
 
     public void setBroker(boolean broker) {
-        isBroker = broker;
+        this.isBroker = broker;
     }
+
+    public Boolean getBroker() {
+        return isBroker;
+    }
+
+    public String toString(){
+        String output = "Username: "+ this.userName + " Password: " + this.password + " Phone number: " + this.phoneNumber;
+        if(this.isBroker){
+            output += " (broker)";
+        }else{
+            output += " (regular user)";
+        }
+        return output;
+    }
+
+
 }
